6fd3e9b6adf287ac32c92f74a841342a8b05af26
Tnkma <onwusilikegodswill@gmail.com>
2023-12-09 04:53:27 +0100

    updates and saves to json

--- models/user.py
+++ models/user.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 """ importing the parent class """
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 class User(BaseModel):
     """
-    A class User that inherites from BaseModel
+    A class User that inherites from BseModel
     """
-    def __init__(self, email, password, first_name, last_name):
-        """
-        initialization of the class
-        """
-        super().__init__()
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
