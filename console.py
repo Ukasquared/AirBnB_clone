25bbb8b53d076a113f69a7f02f5c3c1b187368d2
Tnkma <onwusilikegodswill@gmail.com>
2023-12-09 02:12:20 +0100

    Basic implementation of the console

--- console.py
+++ console.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 """ importing the cmd module"""
 import cmd
-from modes import nonint_mode
 
-class DConsole(cmd.Cmd):
-    """create create your data model.
-    manage (create, update, destroy, etc) objects via a console / command interpreter.
-    store and persist objects to a file (JSON file)."""
-    
-    def do_hello(self, message):
-        print("hello," + message)
-    
-    def do_EOF(self, eof):
+class HBNBCommand(cmd.Cmd):
+    """
+    Basic implementation of the console
+    """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """End of file used to quit the program"""
         print('\n')
         return True
 
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """This wont allow the last excecuted command to repeat 
+        when the ENTER key is pressed"""
+
 if __name__ == "__main__":
-    nonint_mode(DConsole, cmd.Cmd.cmdloop, cmd.Cmd.onecmd)
+    HBNBCommand().cmdloop()
