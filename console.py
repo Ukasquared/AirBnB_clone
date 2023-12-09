78197350c795a99577b2c0a17b846b3edc7b253e
Ukasquared <ukachukwujudith95@gmail.com>
2023-12-09 02:25:51 +0100

    cosole

--- console.py
+++ console.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 """ importing the cmd module"""
 import cmd
-from modes import nonint_mode
 
-class DConsole(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
     """create create your data model.
     manage (create, update, destroy, etc) objects via a console / command interpreter.
     store and persist objects to a file (JSON file)."""
-    
-    def do_hello(self, message):
-        print("hello," + message)
+    prompt = "(hbnb) "
+    def do_quit(self, command):
+        exit(1)
+
+    def emptyline(self):
+        pass
     
     def do_EOF(self, eof):
-        print('\n')
+        print()
         return True
 
 if __name__ == "__main__":
-    nonint_mode(DConsole, cmd.Cmd.cmdloop, cmd.Cmd.onecmd)
+    HBNBCommand().cmdloop()
