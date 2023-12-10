96eec439d04f67fe86728dde9fcaec5031a0e48b
Tnkma <onwusilikegodswill@gmail.com>
2023-12-10 14:56:27 +0100

    updated more classes

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" importing the class """
+from model.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    inherit from basemodel
+    """
+    # createing a public class attributes
+    # All has to be empty
+    place_id = ""
+    user_id = ""
+    text = ""
+
